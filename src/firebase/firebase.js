32ba248dc315bf316066d22937536e09a4787303
EHAB3x <ehabmah6969@gmail.com>
2024-04-19 07:23:38 +0200

    install firebase and use authentication service

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbcj0Rztp01GZK0opBt0tQIzEoPA3gqvY",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
