b4b24b1d426cb11d5442c5786cdcf20a0510f944
EHAB3x <ehabmah6969@gmail.com>
2024-04-19 07:19:27 +0200

    initialize firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbcj0Rztp01GZK0opBt0tQIzEoPA3gqvY",
+  authDomain: "hobbyhorizon-7a49c.firebaseapp.com",
+  projectId: "hobbyhorizon-7a49c",
+  storageBucket: "hobbyhorizon-7a49c.appspot.com",
+  messagingSenderId: "594032617898",
+  appId: "1:594032617898:web:d75d73cccbdcdc9c7263d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
