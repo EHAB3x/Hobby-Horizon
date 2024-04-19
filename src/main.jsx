7656bb5c877db6e259a468cbc7b9c386ba16fa7a
EHAB3x <ehabmah6969@gmail.com>
2024-04-19 08:04:25 +0200

    set global styles and add assets of the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext.js'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>,
+    </HashRouter>,
   </AuthProvider>
 )
