2e7d352eea8355e9b382473ef77a2e46e21160d1
EHAB3x <ehabmah6969@gmail.com>
2024-04-25 21:57:04 +0200

    fix privacy responsive issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import Landing from '../../components/landing/Landing'
 import Categories from '../../components/categories/Categories'
 import Team from '../../components/team/Team'
 import Features from '../../components/features/Features'
-import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
